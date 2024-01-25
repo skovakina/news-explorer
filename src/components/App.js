6e74c0068d51ccbbfa4f98a92c2102e53b53b95d
Svetlana Kovakina <skovakina@gmail.com>
2024-01-25 10:24:00 -0800

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+// import { BrowserRouter, Route, Switch } from 'react-router-dom';
+// import { useState, useEffect } from 'react';
+import React from 'react';
+//  variables
+
+//  components
+
+//  context
+
+//css
+import '../blocks/App.css';
+
+function App() {
+  return <div className="App">hello world</div>;
+}
+
+export default App;
