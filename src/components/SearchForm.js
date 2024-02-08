1e73bc98044a2beeb43f3af330ddd0924283ca5c
Svetlana Kovakina <skovakina@gmail.com>
2024-02-07 16:22:01 -0800

    feat: Search Validation

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,27 +1,37 @@
 import { React, useState } from 'react';
+import { useFormValidation } from './UseFormValidation';
 import '../blocks/SearchForm.css';
 
 export default function SearchForm({ handleSearch }) {
   const [value, setValue] = useState('');
-
-  const handleChange = (event) => {
-    setValue(event.target.value);
-  };
+  const formValidator = useFormValidation();
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    handleSearch(value);
+    handleSearch(formValidator.values['search']);
   };
 
   const isSearched = value.length > 0 ? 'search__input search__input_active' : 'search__input';
   return (
     <div className="search__container">
       <form className="search__box" onSubmit={handleSubmit}>
-        <input name="search" type="text" className={isSearched} placeholder="Enter topic" onChange={handleChange} />
+        <input
+          novalidate
+          required
+          name="search"
+          type="text"
+          className={isSearched}
+          placeholder="Enter topic"
+          value={formValidator.values['search'] || ''}
+          onChange={formValidator.handleChange}
+          isValid={formValidator.isValid}
+        />
+
         <button className="search__button" type="submit">
           Search
         </button>
       </form>
+      <span className="search-input__error search-title-error">{formValidator.errors['search'] && 'Please enter a keyword'}</span>
     </div>
   );
 }
